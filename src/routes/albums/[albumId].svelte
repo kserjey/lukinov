1070ae6560ff41975a76b5c63ebb5036c593771e
Sergey Korchinskiy <k.serjey@gmail.com>
2020-08-07 22:36:12 +0300

    chore: global 100% height

--- src/routes/albums/[albumId].svelte
+++ src/routes/albums/[albumId].svelte
@@ -18,10 +18,6 @@
 
 <style>
   .album {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
     height: 100%;
     box-sizing: border-box;
     display: flex;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,9 +26,6 @@
 	}
 
 	.background {
-		position: fixed;
-		top: 0;
-		left: 0;
 		display: flex;
 		width: 100%;
 		height: 100%;
