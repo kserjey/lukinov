a811b155ee996d60940a649a783dd8c81d27c2dc
Sergey Korchinskiy <k.serjey@gmail.com>
2020-11-19 02:05:38 +0300

    fix: fit images

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,15 +27,14 @@
 
   .background {
     display: flex;
+    align-items: center;
     width: 100%;
     height: 100%;
+    overflow: hidden;
   }
 
   .background img {
-    flex: 1;
     max-width: 100%;
-    height: auto;
-    object-fit: cover;
   }
 
   .bottom-button {
