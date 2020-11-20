23477e4d460a913b1bc2fb32a5b49045f60b6f56
Sergey Korchinskiy <k.serjey@gmail.com>
2020-11-20 23:37:04 +0300

    feat: api key

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,5 +1,6 @@
 import Prismic from 'prismic-javascript';
 
 const apiEndpoint = 'https://lukinov.cdn.prismic.io/api/v2';
+const accessToken = process.env.API_KEY;
 
-export const client = Prismic.client(apiEndpoint);
+export const client = Prismic.client(apiEndpoint, { accessToken });
