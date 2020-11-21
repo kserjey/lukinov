7ddcb5196c2735a4ea017650724342860bc38c79
Sergey Korchinskiy <k.serjey@gmail.com>
2020-11-21 06:35:06 +0300

    feat: orderings

--- src/routes/albums/index.json.js
+++ src/routes/albums/index.json.js
@@ -3,7 +3,8 @@ import { client } from '../../utils/client';
 
 export async function get(req, res, next) {
   const { results } = await client.query(
-    Prismic.Predicates.at('document.type', 'album')
+    Prismic.Predicates.at('document.type', 'album'),
+    { orderings: '[my.album.date desc]' }
   );
 
   res.writeHead(200, { 'Content-Type': 'application/json' });
