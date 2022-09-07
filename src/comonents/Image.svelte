7a06b7f4fd8637bdbbac378dd2ec4e7a178c5c99
Sergei Korchinskii <k.serjey@gmail.com>
2022-09-08 02:55:08 +0300

    feat: Image component

---
+++ src/comonents/Image.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  interface Photo {
+    dimensions: {
+      width: number;
+      height: number;
+    };
+    alt: null | string;
+    copyright: null;
+    url: string;
+  }
+
+  export let photo: Photo;
+  export let srcset: string;
+</script>
+
+<style>
+  img {
+    max-inline-size: 100%;
+    block-size: auto;
+    background: var(--grey);
+  }
+</style>
+
+<img
+  width={photo.dimensions.width}
+  height={photo.dimensions.height}
+  alt={photo.alt}
+  {srcset}
+/>
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
+import sveltePreprocess from 'svelte-preprocess';
 
+/**
+ * @type {import('@sveltejs/kit').Config}
+ */
 export default {
   kit: {
     adapter: adapter(),
   },
+  preprocess: sveltePreprocess(),
 };
